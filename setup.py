4263c1dab048551ad2a5761fb349755bb9d7139c
dbf256 <dbf256@9f7d463a-1175-7cec-4b28-25d74534fe60>
2014-10-23 20:48:37 +0000

    Code structure was made compatible with python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyyaxml',
+      version='0.5',
+      description='Python API to Yandex.XML',
+      url='https://code.google.com/p/py-ya-xml/',
+      author='Alexey Moskvin',
+      author_email='dbf256@gmail.com',
+      license='MIT',
+      packages=['pyyaxml'],
+      zip_safe=False)
